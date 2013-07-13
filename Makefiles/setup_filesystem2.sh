bb439fde12b8dbd0ad69955c4cc648289736515b
Laura Lindzey <lindzey@gmail.com>
2013-07-13 17:06:44 -0400

    and, updating my Makefile notebook ... still can't figure out more advanced dependency generations

--- Makefiles/setup_filesystem2.sh
+++ Makefiles/setup_filesystem2.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/env bash
 
-fs=filesystem1
+fs=filesystem2
 mkdir -p $fs
 
 for year in 2003 2004 2005; do
@@ -9,5 +9,9 @@ for year in 2003 2004 2005; do
     for month in jan feb march; do
 	mkdir -p $fs/$year/$month
 	touch $fs/$year/$month/data.input
+	touch $fs/$month.dat
     done
+    
+    touch $fs/$year/stats.txt
 done
+
