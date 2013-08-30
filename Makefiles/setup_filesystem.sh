25c80406810fe20360c58a3cb9713a7de988be6d
Laura Lindzey <lindzey@gmail.com>
2013-08-30 01:07:05 -0400

    cleaning up my makefile notebook

---
+++ Makefiles/setup_filesystem.sh
@@ -0,0 +1,17 @@
+#! /usr/bin/env bash
+
+fs=filesystem
+mkdir -p $fs
+
+for year in 2003 2004 2005; do
+    mkdir -p $fs/$year
+    
+    for month in jan feb march; do
+	mkdir -p $fs/$year/$month
+	touch $fs/$year/$month/data.input
+	touch $fs/$month.dat
+    done
+    
+    touch $fs/$year/stats.txt
+done
+
